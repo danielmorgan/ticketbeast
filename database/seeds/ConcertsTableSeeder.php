693b10a846195dcce057a8d43a934e25a1e69796
Daniel Morgan <me@danielmorgan.co.uk>
2017-03-01 21:33:33 +0000

    Add concert listing (TDL_1.3)

---
+++ database/seeds/ConcertsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Concert;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class ConcertsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Concert::create([
+            'title' => 'The Red Chord',
+            'subtitle' => 'with Animosity and Lethargy',
+            'date' => Carbon::parse('December 13, 2016 8:00pm'),
+            'ticket_price' => 3250,
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '17916',
+            'additional_information' => 'For tickets, call (555) 555-5555.',
+        ]);
+    }
+}
