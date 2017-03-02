0462fb62690e82bd18bdc303baa7a01b3f10b25f
Daniel Morgan <me@danielmorgan.co.uk>
2017-03-02 13:50:14 +0000

    Add some styles

--- resources/views/concerts/show.blade.php
+++ resources/views/concerts/show.blade.php
@@ -1,9 +1,15 @@
-<h1>{{ $concert->title }}</h1>
-<h2>{{ $concert->subtitle }}</h2>
-<p>{{ $concert->formatted_date }}</p>
-<p>Doors at {{ $concert->formatted_start_time }}</p>
-<p>&pound;{{ $concert->ticket_price_in_gbp }}</p>
-<p>{{ $concert->venue }}</p>
-<p>{{ $concert->venue_address }}</p>
-<p>{{ $concert->city }}, {{ $concert->state }} {{ $concert->zip }}</p>
-<p>{{ $concert->additional_information }}</p>
+@extends('layouts.app')
+
+@section('content')
+    <div class="Card">
+        <h1>{{ $concert->title }}</h1>
+        <h2>{{ $concert->subtitle }}</h2>
+        <p>{{ $concert->formatted_date }}</p>
+        <p>Doors at {{ $concert->formatted_start_time }}</p>
+        <p>&pound;{{ $concert->ticket_price_in_gbp }}</p>
+        <p>{{ $concert->venue }}</p>
+        <p>{{ $concert->venue_address }}</p>
+        <p>{{ $concert->city }}, {{ $concert->state }} {{ $concert->zip }}</p>
+        <p>{{ $concert->additional_information }}</p>
+    </div>
+@endsection
